bd082e05fd88c9f33c22e3ccc955ce9e710e5888
GopalBankey <gopal.gmtechnosys@gmail.com>
2025-12-05 10:28:59 +0530

    Gopal Push

--- src/main/java/com/gmvehicleinout/controller/EntryController.java
+++ src/main/java/com/gmvehicleinout/controller/EntryController.java
@@ -4,7 +4,10 @@ import com.gmvehicleinout.dto.EntryDto;
 import com.gmvehicleinout.dto.EntryResponseDto;
 import com.gmvehicleinout.entity.ApiResponse;
 import com.gmvehicleinout.service.EntryService;
-import jakarta.validation.Valid;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -20,28 +23,31 @@ public class EntryController {
     @Autowired
     private EntryService entryService;
 
-    @PostMapping(value = "/in-entry", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(
+            value = "/in-entry",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+    )
     public ResponseEntity<ApiResponse<EntryResponseDto>> addEntry(
-            @RequestPart("data") EntryDto entryDto,     // JSON OBJECT
+
+            @RequestPart("data") String dataJson,
             @RequestPart(value = "rcPhoto", required = false) MultipartFile rcPhoto,
             @RequestPart(value = "vehiclePhoto", required = false) MultipartFile vehiclePhoto,
             @RequestPart(value = "idCardPhoto", required = false) MultipartFile idCardPhoto
-    ) {
+    ) throws JsonProcessingException {
+
+        ObjectMapper mapper = new ObjectMapper();
+        EntryDto entryDto = mapper.readValue(dataJson, EntryDto.class);
+
         return entryService.addEntry(entryDto, rcPhoto, vehiclePhoto, idCardPhoto);
     }
 
-
     @PostMapping("/out-entry/{vehicleNumber}")
     public ResponseEntity<ApiResponse> outEntry(@PathVariable String vehicleNumber) {
-
         return entryService.outEntry(vehicleNumber);
-
     }
 
     @GetMapping("/all")
     public ResponseEntity<ApiResponse<List<EntryResponseDto>>> getAllEntries() {
         return entryService.getAllEntry();
     }
-
-
 }
